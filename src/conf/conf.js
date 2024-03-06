1d9158be6acb22cf293b4ceb6084375c71fef2d9
Meghavati Chaudhari <meghapjc29@gmail.com>
2024-03-06 17:37:00 +0530

    gitignore changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf={
-    appwriteurl:String(" "),
-    appwriteProjectId:String(" "),
-    appwriteDatabaseId:String(" "),
-    appwriteCollectionId:String(" "),
-    appwriteBucketId:String(" "),
+    appwriteurl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 
 }
 
