8ed5ab450c3bd2a5fe3909e93f5b9a721a12b0eb
Meghavati Chaudhari <meghapjc29@gmail.com>
2024-03-07 23:55:08 +0530

    components forms added

---
+++ src/components/Plogo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Plogo({width='100px'}){
+    return(
+        <div>logo</div>
+    )
+}
+
+export default Plogo;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,18 @@
 import Header from './Header/Header'
-import Footer from './Footer/Footer'
+import Footer from './Footer/Footer';
+import Container from './container/Container';
+import Plogo from './Plogo';
+import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import Input from './Input';
+import RTE from './Input';
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Plogo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE
 }
